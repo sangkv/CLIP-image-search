a378a42c13d7ef37097b3800eb6c23674fb67e44
sangkv <sangkv.work@gmail.com>
2022-09-09 13:17:37 +0700

    extract features

--- features.py
+++ features.py
@@ -1,39 +1,34 @@
 import torch
 import clip 
-import numpy as np
 
-class features():
-    def __init__(self, model="ViT-B/32"):
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model, self.preprocess = clip.load(model, device=self.device)
+class extract_features():
+    def __init__(self, model='ViT-B/32'):
+        # Load the model
+        self.DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model, self.preprocess = clip.load(model, device=self.DEVICE)
 
-    def extract_image(self, image):
-        image = self.preprocess(image).unsqueeze(0).to(self.device)
+    def extract_image_features(self, image):
+        # Prepare the image
+        image_input = self.preprocess(image).unsqueeze(0).to(self.DEVICE)
 
+        # Calculate features of the image
         with torch.no_grad():
-            image_features = self.model.encode_image(image)
+            image_features = self.model.encode_image(image_input)
         
+        # Normalize
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+
         return image_features
 
-    def extract_text(self, text_list):
-        text = clip.tokenize(text_list).to(self.device)
+    def extract_text_features(self, list_text):
+        # Prepare the text
+        text_inputs = clip.tokenize(list_text).to(self.DEVICE)
 
+        # Calculate features of the image
         with torch.no_grad():
-            text_features = self.model.encode_text(text)
+            text_features = self.model.encode_text(text_inputs)
         
+        # Normalize
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+
         return text_features
-    
-    def classifier(self, img, text_list):
-        image = self.preprocess(img).unsqueeze(0).to(self.device)
-        text = clip.tokenize(text_list).to(self.device)
-        with torch.no_grad():
-            logits_per_image, logits_per_text = self.model(image, text)
-
-            probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-        
-        idx = np.argmax(probs)
-        prob = np.max(probs)
-
-        return idx, prob
-
-
---
+++ search.py
@@ -0,0 +1,17 @@
+from statistics import mode
+from PIL import Image
+from features import extract_features
+
+# Load model
+model = extract_features()
+
+# extract
+image = Image.open('Corel-1000/16.jpg')
+image_features = model.extract_image_features(image)
+
+text = 'man'
+text_features = model.extract_text_features(text)
+
+print('image features: ', image_features)
+print('features_text: ', text_features)
+
